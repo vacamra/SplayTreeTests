c0bc90e65eb5a6b8d12c32c9d597c7a7136cd7eb
Václav Čamra <vacamra@gmail.com>
2016-05-03 21:51:45 +0200

    Added tree consistency test
    Signed-off-by: Václav Čamra <vacamra@gmail.com>

--- SplayTreeConcurrent/testingUtils.h
+++ SplayTreeConcurrent/testingUtils.h
@@ -2,12 +2,22 @@
 #include <iostream>
 #include <concurrent_unordered_set.h>
 #include <unordered_set>
+//Don't try this at home - for root access
+#define private public 
 #include "splayset_concurrent.hpp"
 
 inline void my_assert(bool expresssion)
 {
 	if (!expresssion) {
 		std::cout << "Problem" << std::endl;
+	}
+}
+
+inline void my_assert(bool expression, char* message)
+{
+	if (!expression)
+	{
+		std::cout << "Problem: " << message << std::endl;
 	}
 }
 
@@ -29,4 +39,23 @@ void testUSnSt(const std::unordered_set<T> us, splayset<T>& st)
 		my_assert(res && res->data == item);
 	}
 }
+
+//Expects node to have parent
+template <typename T>
+size_t subTreeCountAndCheckParent(typename splayset<T>::node* node)
+{
+	if (!node) return 0;
+	my_assert(node->P->L == node || node->P->R == node, "father doesn't recognize his son");
+	return subTreeCountAndCheckParent<T>(node->L) + 1 + subTreeCountAndCheckParent<T>(node->R);
+}
+
+template <typename T>
+void testTreeConsistency(splayset<T>& st, size_t expectedNodeCount)
+{
+	using node = typename splayset<T>::node;
+	node* root = st.root;
+	size_t count = subTreeCountAndCheckParent<T>(root->L) + 1 + subTreeCountAndCheckParent<T>(root->R);
+	my_assert(count == expectedNodeCount, "node count doesn't match");
+}
+
 
